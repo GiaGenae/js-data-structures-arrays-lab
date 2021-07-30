5e59a54a27c9a9b19bba30c06fa806b29211c339
Gia Jennings <gjennings.co@gmail.com>
2021-07-29 20:41:42 -0400

    commit tree 45cc50e3123ceee208ae7595f4ebcbee1c39887b

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+
+var drivers = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendDriver(name){
+    return drivers.push("Ralph")
+}
+
+function destructivelyPrependDriver(name){
+    return drivers.unshift("Bob")
+}
+
+function destructivelyRemoveLastDriver(name){
+    return drivers.pop()
+}
+
+function destructivelyRemoveFirstDriver(name){
+    return drivers.shift()
+}
+
+function appendDriver(name){
+    return [...drivers, "Broom"]
+}
+
+function prependDriver(name){
+    return ["Arnold", ...drivers]
+}
+
+function removeLastDriver(name){
+    return drivers.slice(0,drivers.length-1)
+}
+
+function removeFirstDriver(name){
+    return drivers.slice(1)
+}
